3498dd880c61961d277131401598a07d45f1fe97
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-20 19:28:25 +0330

    graphQL was set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      My app
-    </div>
-  );
+  return <div className="App">My app</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl9d20lhg0noi01uq2zc90wlj/master",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
 );
