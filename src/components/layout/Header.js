fdcbad8b1f121c924fe79a35cdcdeb72e9be3319
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-20 20:47:41 +0330

    AppBar created

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import React from "react";
+
+// assets
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 const Header = () => {
-   return (
-      <div>
-         
-      </div>
-   );
+  return (
+    <AppBar position="sticky">
+      <Container maxWidth='lg'>
+        <Toolbar>
+          <Typography component="h1" variant="h5" fontWeight="bold" flex={1}>
+            Quoteblog
+          </Typography>
+          <MenuBookIcon />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
