8b8534462c6b3306aea9493caca16fe64dbe4799
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-21 12:40:59 +0330

    The queries were written

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+
+import { useQuery } from "@apollo/client";
+import { GET_BLOGS_INFO } from "../../graphql/queries";
 
 const Blogs = () => {
-   return (
-      <div>
-         Blogs
-      </div>
-   );
+  const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
+
+  if(loading) return <h4>Loading ...</h4>
+  if(errors) return <h4>Error</h4>
+  return <div>Blogs</div>;
 };
 
 export default Blogs;
---
+++ src/graphql/queries.js
@@ -0,0 +1,22 @@
+const { gql } = require("@apollo/client");
+
+const GET_BLOGS_INFO = gql`
+  query {
+    posts {
+      author {
+        name
+        avatar {
+          url
+        }
+      }
+      title
+      slug
+      id
+      coverPhoto {
+        url
+      }
+    }
+  }
+`;
+
+export { GET_BLOGS_INFO };
