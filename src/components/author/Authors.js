f4321556214136ec27dbb8efe98c13e0b5bc679d
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-21 13:54:47 +0330

    Getting the data of the authors

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+
+import { useQuery } from "@apollo/client";
+import { GET_AUTHORS_INFO } from "../../graphql/queries";
 
 const Authors = () => {
-   return (
-      <div>
-         authors
-      </div>
-   );
+  const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+
+  if (loading) return <h3>Loading ...</h3>;
+
+  if (errors) return <h3>Error</h3>;
+  
+  return <div>authors</div>;
 };
 
 export default Authors;
