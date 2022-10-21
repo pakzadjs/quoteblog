7688c7f25321fff8200c5fdd2f5051b17aaa896a
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-21 11:06:41 +0330

    Arrangement of home page elements and making it responsive

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,31 @@
+import { Container, Grid, Typography } from "@mui/material";
+import React from "react";
+
+// Components
+import Authors from "../author/Authors";
+import Blogs from "../blog/Blogs";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Container maxWidth="lg">
+        <Grid container spacing={2} padding={3}>
+          <Grid item xs={12} md={3} mt={4}>
+            <Typography component="h3" variant="h5" mb={3} fontWeight={700}>
+              نویسنده ها
+            </Typography>
+            <Authors />
+          </Grid>
+          <Grid item xs={12} md={9} mt={4}>
+            <Typography component="h3" variant="h5" mb={3} fontWeight={700}>
+              مقالات
+            </Typography>
+            <Blogs />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
