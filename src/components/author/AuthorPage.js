5383e6c95c7482f2413f40d5a06de593cae24b2c
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-22 17:42:05 +0330

    author page

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 import { GET_AUTHOR_INFO } from "../../graphql/queries";
+
+import { Avatar, Container, Grid, Typography } from "@mui/material";
 
 const AuthorPage = () => {
   const { slug } = useParams();
@@ -16,8 +18,32 @@ const AuthorPage = () => {
   if (errors) return <h3>Error</h3>;
 
   console.log(data);
+  const { avatar, name, field } = data.author;
 
-  return <div>Author page</div>;
+  return (
+    <Container maxWidth="lg">
+      <Grid container mt={10}>
+        <Grid
+          item
+          xs={12}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Avatar src={avatar.url} sx={{ width: 250, height: 250 }} />
+          <Typography component="h3" variant="h5" fontWeight={700} mt={2}>
+            {name}
+          </Typography>
+          <Typography component="p" variant="h5">
+            {field}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {data.author.description.html}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default AuthorPage;
