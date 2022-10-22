a21fd26f97595326e6caa64ba8fe76cdcedfd4c0
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-22 19:05:35 +0330

    The data of the articles was taken

--- src/components/blog/BlogPage.js
+++ src/components/blog/BlogPage.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+
+// Components
+import Loader from "../shared/Loader";
+
+// GraphQL
+import { useQuery } from "@apollo/client";
+import { GET_POST_INFO } from "../../graphql/queries";
 
 const BlogPage = () => {
-   return (
-      <div>
-         blog page
-      </div>
-   );
+  const { slug } = useParams();
+
+  const { loading, data, errors } = useQuery(GET_POST_INFO, {
+    variables: { slug },
+  });
+
+  if (loading) return <Loader />;
+
+  if (errors) return <h3>Error</h3>;
+
+  console.log(data);
+  return <div>blog page</div>;
 };
 
 export default BlogPage;
