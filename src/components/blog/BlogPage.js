9832df4b3f6f661e93bf652b21d4d09ad5287f38
Hamid Pakzad <pakzadlive@gmail.com>
2022-10-22 19:45:04 +0330

    blog page

--- src/components/blog/BlogPage.js
+++ src/components/blog/BlogPage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import sanitizeHtml from "sanitize-html";
 
 // Components
 import Loader from "../shared/Loader";
@@ -8,8 +9,14 @@ import Loader from "../shared/Loader";
 import { useQuery } from "@apollo/client";
 import { GET_POST_INFO } from "../../graphql/queries";
 
+// MaterialUI
+import { Box, Container } from "@mui/system";
+import { Avatar, Grid, Typography } from "@mui/material";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+
 const BlogPage = () => {
   const { slug } = useParams();
+  const navigate = useNavigate();
 
   const { loading, data, errors } = useQuery(GET_POST_INFO, {
     variables: { slug },
@@ -19,8 +26,56 @@ const BlogPage = () => {
 
   if (errors) return <h3>Error</h3>;
 
-  console.log(data);
-  return <div>blog page</div>;
+  const { coverPhoto, title, author, content } = data.post;
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid item xs={12} mt={9} display="flex" justifyContent="space-between">
+          <Typography
+            component="h2"
+            variant="h4"
+            color="primary"
+            fontWeight={700}
+          >
+            {title}
+          </Typography>
+          <ArrowBackRoundedIcon
+            onClick={() => navigate(-1)}
+            style={{ cursor: "pointer" }}
+          />
+        </Grid>
+        <Grid item xs={12} mt={6}>
+          <img
+            src={coverPhoto.url}
+            alt={data.post.slug}
+            width="100%"
+            style={{ borderRadius: "15px" }}
+          />
+        </Grid>
+        <Grid item xs={12} mt={7} display="flex" alignItems="center">
+          <Avatar
+            src={author?.avatar?.url}
+            sx={{ width: 80, height: 80, marginLeft: 2 }}
+          />
+
+          <Box component="div">
+            <Typography component="p" variant="h5" fontWeight={700}>
+              {author.name}
+            </Typography>
+            <Typography component="p" variant="p" color="text.secondary">
+              {author.field}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} mt={5}>
+          <div
+            dangerouslySetInnerHTML={{ __html: sanitizeHtml(content.html) }}
+          ></div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default BlogPage;
